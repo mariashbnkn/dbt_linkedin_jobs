61290b87b97414657bbcfb9bd0f0a1a6c72d3404
mary <maria.shubenkina@yandex.ru>
2025-05-15 13:37:47 +0300

    first

---
+++ pg_dbt/models/jobs/staging/stg_jobs.sql
@@ -0,0 +1,18 @@
+-- {{ config(materialized='view') }}
+
+SELECT
+    job_link,
+    job_title,
+    company,
+    job_location,
+    first_seen,
+    search_city,
+    search_country,
+    search_position,
+    job_level,
+    job_type,
+    -- Дополнительные преобразования
+    SPLIT_PART(job_location, ', ', 1) AS job_city,
+    SPLIT_PART(job_location, ', ', 2) AS job_state,
+    DATE(first_seen) AS first_seen_date
+FROM {{ source('dbt', 'raw_jobs') }}
